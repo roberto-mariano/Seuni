41602d217deb0d629500783c41d0f314a847aa5c
roberto-mariano <robertomariano@uern.br>
2025-06-10 17:20:11 -0300

    Correção de identação no código

--- seuni.py
+++ seuni.py
@@ -50,10 +50,8 @@ def formulario():
     erros = {}
     vagas_ocupadas = contar_vagas()
 
-    if request.method == 'POST':
-        # -----------------------
-        # 1) Campos comuns
-
-if __name__=='__main__':
-    port = int(os.environ.get("PORT",5000))
-    app.run(host='0.0.0.0', port=port)
+    if __name__ == '__main__':
+        # Railway injeta a variável PORT; usa 5000 localmente
+        import os
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port)
